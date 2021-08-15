94fac587af96f99c8313e16232d7e0aa2307d335
gq <guanquntan@gmail.com>
2021-08-15 23:24:11 +0800

    errors bye

--- DM2212_Physics/Physics/Source/GameObject.h
+++ DM2212_Physics/Physics/Source/GameObject.h
@@ -17,9 +17,26 @@ struct GameObject
 		GO_WALL, // INELASTIC
 		GO_PILLAR, // INELASTIC
 
+		GO_FLIPPER,
+		GO_SPRING, // ELASTIC + MOVING
+		GO_MWALL, // INELASTIC(0.5x) + MOVING
+		GO_MPILLAR, // INELASTIC(0.5x) + MOVING
+		GO_ONESIDEDWALL, // INELASTIC(0.5x) + ONESIDED
+		GO_MARKER, // NO COLLISION
+		GO_BONUS, // ELASTIC + free coloured balls from the top
+		GO_RWALL, // INELASTIC + ROTATING
+		GO_RPILLAR, // INELASTIC + ROTATING
+		GO_BWALL, // ELASTIC + MOVING
+		GO_EPILLAR, // ELASTIC
+		GO_BPILLAR, // BUMPER(2.0x) + MOVING
+		GO_100, // ELASTIC + SCORE
+		GO_50, // ELASTIC + SCORE
+		GO_10, // ELASTIC + SCORE
+
+		GO_HIGHLIGHT,
+
 		GO_TOTAL, //must be last
 	};
-
 	GAMEOBJECT_TYPE type;
 	bool active;
 	Vector3 pos;
@@ -34,6 +51,11 @@ struct GameObject
 	float angularVelocity;
 	float mass;
 
+	double fireInterval;
+	int maxHP, currentHP;
+	double timeout;
+
+	Material mat;
 
 
 	GameObject(GAMEOBJECT_TYPE typeValue = GO_BALL);
