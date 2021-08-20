6e5985729ab1ca88d5f104d070895e38ef89f937
Jordan-low <jawdanlow@gmail.com>
2021-08-20 12:16:25 +0800

    got my jungle tiles and scene working

--- DM2212_Physics/Physics/Source/SceneManager.cpp
+++ DM2212_Physics/Physics/Source/SceneManager.cpp
@@ -35,6 +35,9 @@ void SceneManager::setScene(worlds sceneType)
 	case w_physics:
 		activeScene = physics;
 		break;
+	case w_jungle:
+		activeScene = jungle;
+		break;
 	case w_graveyard:
 		activeScene = graveyard;
 		break;
