cd5c6a332c2df68a492195afd79aacaeb2cc98aa
Jordan <jawdanlow@gmail.com>
2021-08-29 17:57:40 +0800

    d

--- DM2212_Physics/Physics/Source/SceneJungle.cpp
+++ DM2212_Physics/Physics/Source/SceneJungle.cpp
@@ -314,7 +314,7 @@ void SceneJungle::Exit()
 	//Cleanup GameObjects
 	goManager->Exit();
 	inventory->Clear();
-	CSoundController::GetInstance()->StopPlayingSoundByID(SOUND_TYPE::BG_JUNGLE);
+	CSoundController::GetInstance()->StopPlayingSoundByID(SOUND_TYPE::BG_JUNGLE, 1, 0.5);
 }
 
 void SceneJungle::InitLights()
